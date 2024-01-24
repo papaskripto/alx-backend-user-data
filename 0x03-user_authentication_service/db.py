a442b0078b2cdb1d0b59fe911e26a525089e40b5
papaskripto <bygeoffreyouma@gmail.com>
2024-01-24 17:44:17 +0300

    Add find user by id method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 from user import Base, User
+from typing import TypeVar
+
+
+ALLOWED_FIELDS = {'id', 'email', 'hashed_password', 'user_id', 'reset_token'}
 
 
 class DB:
@@ -41,3 +47,25 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        ''' Finds user by id.
+        '''
+        if not kwargs or any(i not in ALLOWED_FIELDS for i in kwargs):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        ''' Updates user model.
+        '''
+        session = self._session
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in ALLOWED_FIELDS:
+                raise ValueError
+            setattr(user, k, v)
+        session.commit()
